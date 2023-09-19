67cc70c75ba32f1c1f8ce82d0398e0d5cde041b5
DEV-LONNOS <luisfelipedasilvaaraujo18@gmail.com>
2023-09-18 21:51:49 -0300

    Incluir o projeto ao Git.

---
+++ app/src/main/java/com/example/appupdate2/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.appupdate2
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.appupdate2.ui.theme.AppUpdate2Theme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppUpdate2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    var LUISFELLIPE by remember {
+                        mutableStateOf("")
+                    }
+                    Column() {
+                        TextField(value = LUISFELLIPE, onValueChange = {
+                            LUISFELLIPE = it;
+                            Log.i("####", LUISFELLIPE)
+                        })
+                }
+            }
+        }
+    }
+}}
